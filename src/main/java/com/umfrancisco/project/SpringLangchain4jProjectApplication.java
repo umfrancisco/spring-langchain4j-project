336183164ce3546d040a3395602e6aa63e336fea
umfrancisco <francis.guitler@gmail.com>
2026-06-28 11:39:15 -0300

    First commit

---
+++ src/main/java/com/umfrancisco/project/SpringLangchain4jProjectApplication.java
@@ -0,0 +1,12 @@
+package com.umfrancisco.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLangchain4jProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLangchain4jProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/umfrancisco/project/SpringLangchain4jProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umfrancisco.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLangchain4jProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
